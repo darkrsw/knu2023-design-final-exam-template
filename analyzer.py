75903ca0dffbeb60db0b758d622c13d21505f430
Dongsun Kim <darkrsw@gmail.com>
2023-12-19 01:57:56 +0900

    template.

--- analyzer.py
+++ analyzer.py
@@ -1,2 +1,16 @@
+from types import FrameType, TracebackType
+from typing import Any, Optional, Callable
+import sys
+import pytest
+
+def traceit(frame: FrameType, event: str, arg: Any) -> Optional[Callable]:
+    print(frame.f_code.co_filename)
+    print("self" in frame.f_locals and hasattr(frame.f_locals["self"], "a"))
+    print(type(getattr(frame.f_locals["self"], "a")).__name__)
+    return traceit
+
 def collect_variable_types(path: str, target_class: str):
-  return dict()
+    sys.settrace(traceit)
+    pytest.main(["-s", "./src/"])
+    sys.settrace(None)
+    return dict()
